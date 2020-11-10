ba062df45cf1467ffad322599c67909cc2c8d619
R <boruix@icloud.com>
2020-11-09 21:06:12 -0500

    Initial Commit

---
+++ CentralMixBook/CentralMixBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CentralMixBookApp.swift
+//  CentralMixBook
+//
+//  Created by Borui Xu on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct CentralMixBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
